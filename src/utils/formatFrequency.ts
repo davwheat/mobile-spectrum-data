ea6ccae700d1c604f0a238f1d3abfc0c20f7dee7
David Wheatley <hi@davwheat.dev>
2022-07-18 20:36:35 +0100

    feat: add option to hide units in frequency formatting

--- src/utils/formatFrequency.ts
+++ src/utils/formatFrequency.ts
@@ -2,9 +2,21 @@ function roundToPrecision(val: number, precision: number): number {
   return Math.round(val * 10 ** precision) / 10 ** precision;
 }
 
-export function formatFrequency(freqMhz: number): string {
+export function formatFrequency(
+  freqMhz: number,
+  hideUnits: boolean = false
+): string {
   if (freqMhz > 10_000) {
+    if (hideUnits) {
+      return `${roundToPrecision(freqMhz / 1000, 2)}`;
+    }
+
     return `${roundToPrecision(freqMhz / 1000, 2)} GHz`;
   }
+
+  if (hideUnits) {
+    return `${freqMhz}`;
+  }
+
   return `${freqMhz} MHz`;
 }
