d01abce547935020d2deb7dc0dc79bd403c2ef67
David Wheatley <david@davwheat.dev>
2022-10-28 15:18:45 +0100

    chore: complete `BG` -> `Bulgaria` changes

--- src/OperatorInfo/BG.ts
+++ src/OperatorInfo/BG.ts
@@ -3,12 +3,12 @@ import { IOperatorInfo } from ".";
 const operators: IOperatorInfo[] = [
   {
     name: "A1 Bulgaria",
-    aliases: ["A1", "A1 BG"],
+    aliases: ["A1", "A1 Bulgaria"],
     color: "#e70022",
   },
   {
     name: "Yettel Bulgaria",
-    aliases: ["Yettel", "Yettel BG"],
+    aliases: ["Yettel", "Yettel Bulgaria"],
     color: "#b4ff00",
   },
   {
