d00328474737d6d7d1db60b04a047d4e9c65ce61
David Wheatley <david@davwheat.dev>
2023-02-09 23:40:47 +0000

    feat(KR): add south korean spectrum data
    Contributed by @peremen in #7.
    
    Fixes #7

---
+++ src/KR/78.ts
@@ -0,0 +1,42 @@
+import type { SpectrumBlock } from "../@types";
+
+const data: SpectrumBlock[] = [
+  {
+    details: [],
+    endFreq: 3500,
+    owner: "LG U+",
+    sourceInfo: {
+      type: "url",
+      url: "https://spectrummap.kr/radioInfo/radioUseConditionDiagramView.do?menuNo=20601",
+    },
+    startFreq: 3420,
+    type: "tdd",
+  },
+  {
+    details: [],
+    endFreq: 3600,
+    nrarfcns: [636672],
+    owner: "KT",
+    sourceInfo: {
+      type: "url",
+      url: "https://spectrummap.kr/radioInfo/radioUseConditionDiagramView.do?menuNo=20601",
+    },
+    startFreq: 3500,
+    type: "tdd",
+  },
+  {
+    details: [],
+    endFreq: 3700,
+    nrarfcns: [643392],
+    owner: "SKT",
+    ownerLongName: "SK Telecom",
+    sourceInfo: {
+      type: "url",
+      url: "https://spectrummap.kr/radioInfo/radioUseConditionDiagramView.do?menuNo=20601",
+    },
+    startFreq: 3600,
+    type: "tdd",
+  },
+];
+
+export default data;
