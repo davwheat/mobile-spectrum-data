b07bb66ddfb20759fa104f1c29d6c1faf81a4188
David Wheatley <david@davwheat.dev>
2025-06-18 15:32:53 +0100

    feat: export spectrum

--- src/GB/index.ts
+++ src/GB/index.ts
@@ -1,4 +1,7 @@
 import Band1 from "./1";
+import Band1_20250801 from "./1_2025_08_01";
+import Band1_20260101 from "./1_2026_01_01";
+import Band1_20310930 from "./1_2031_09_30";
 import Band3 from "./3";
 import Band3_Merger from "./3_merger";
 import Band7 from "./7";
@@ -7,13 +10,18 @@ import Band8_SpectrumSwap from "./8_swap";
 import Band20 from "./20";
 import Band28 from "./28";
 import Band32 from "./32";
+import Band32_20250801 from "./32_2025_08_01";
+import Band32_20260930 from "./38_2026_09_30";
 import Band38 from "./38";
+import Band38_20250801 from "./38_2025_08_01";
+import Band38_20260930 from "./38_2026_09_30";
 import Band39 from "./39";
 import Band40 from "./40";
 import Band67 from "./67";
 import Band77 from "./77";
 import Band78 from "./78";
 import Band78_SpectrumSwap from "./78_swap";
+import Band78_20260101 from "./78_2026_01_01";
 
 import type { SpectrumData } from "../@types";
 
@@ -21,6 +29,30 @@ const data: SpectrumData[] = [
   {
     names: ["B1", "n1"],
     spectrumData: Band1,
+  },
+  {
+    names: ["B1", "n1"],
+    extraInfo: {
+      shortAddendum: "from 2025-08-01",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band1_20250801,
+  },
+  {
+    names: ["B1", "n1"],
+    extraInfo: {
+      shortAddendum: "from 2026-01-01",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band1_20260101,
+  },
+  {
+    names: ["B1", "n1"],
+    extraInfo: {
+      shortAddendum: "from 2031-09-30",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band1_20310930,
   },
   {
     names: ["B3", "n3"],
@@ -67,8 +99,40 @@ const data: SpectrumData[] = [
     spectrumData: Band32,
   },
   {
+    names: ["B32"],
+    extraInfo: {
+      shortAddendum: "from 2025-08-01",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band32_20250801,
+  },
+  {
+    names: ["B32"],
+    extraInfo: {
+      shortAddendum: "from 2026-09-30",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band32_20260930,
+  },
+  {
     names: ["B38"],
     spectrumData: Band38,
+  },
+  {
+    names: ["B38"],
+    extraInfo: {
+      shortAddendum: "from 2025-08-01",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band38_20250801,
+  },
+  {
+    names: ["B38"],
+    extraInfo: {
+      shortAddendum: "from 2026-09-30",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band38_20260930,
   },
   {
     names: ["B39"],
@@ -99,6 +163,14 @@ const data: SpectrumData[] = [
       excludeFromSpectrumTotal: true,
     },
     spectrumData: Band78_SpectrumSwap,
+  },
+  {
+    names: ["n78"],
+    extraInfo: {
+      shortAddendum: "from 2026-01-01",
+      excludeFromSpectrumTotal: true,
+    },
+    spectrumData: Band78_20260101,
   },
 ];
 
